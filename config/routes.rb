42651b99faba950fa7d48e296bdacb93077969ce
shinya524 <gheena1800@gmail.com>
2022-08-09 03:09:45 +0000

    [Add]4-25 投稿フォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
-  
+
+
   devise_for :users
   root to: "homes#top"
-  
+  get 'homes/about' => 'homes#about', as: 'about'
+
+  resource :books, only: [:new, :index, :show, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
