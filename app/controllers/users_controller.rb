7f5c6d5724f9c13a3b4518a99e2b1a6d113c3f48
shinya524 <gheena1800@gmail.com>
2022-08-09 09:51:29 +0000

    [Add]4-25 ユーザー一覧ページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @user = User.all
+  end
+
   def edit
     @user = User.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
 
   resources :books, only: [:new, :index, :show, :edit, :create, :destroy]
-  resources :users, only: [:edit, :show, :update, :destroy]
+  resources :users, only: [:index, :edit, :show, :update, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
