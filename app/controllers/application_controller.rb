e05cdba15f743ca2acb619e1cde81ba139211fa9
shinya524 <gheena1800@gmail.com>
2022-08-13 00:25:25 +0000

    [Add]4-25 名前ログインの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
 end
